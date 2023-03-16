2fd4e4a136beebececd3445fd2e719c04f9a2a95
Kostya Kosyuk <kostya.kosyuk007@gmail.com>
2023-03-16 20:51:26 +0200

    added interactive background, sprite. Added information window

---
+++ src/Objects/Room.ts
@@ -0,0 +1,88 @@
+import { Container, Graphics, Sprite, Texture, Text } from "pixi.js";
+import { Coords } from "./Coords";
+
+export class Room extends Container {
+    title: string;
+    coords: Coords;
+    sprite: Sprite;
+    infoWindow: Container;
+    constructor (
+        title: string,
+        width: number,
+        height: number,
+        x: number,
+        y: number
+    ) {
+        super();
+        this.title = title;
+        this.width = width;
+        this.height = height;
+
+        this.coords = this.addCoords(x, y);
+
+        this.sprite = this.addSprite(width, height);
+
+        this.infoWindow = this.addInfoWindow();
+
+        this.addBorder(this.sprite);
+        this.addBorder(this.infoWindow);
+
+        this.showinfo();
+    };
+
+    addCoords(x: number, y: number) {
+        const coords = new Coords(x, y);
+
+        this.position.x = coords.x;
+        this.position.y = coords.y;
+
+        return coords;
+    };
+
+    addSprite(width: number, height: number) {
+        const sprite = new Sprite(Texture.WHITE);
+
+        sprite.alpha = 0;
+        sprite.width = width;
+        sprite.height = height;
+
+        sprite.interactive = true;
+        sprite.on('click', this.onClickSprite);
+
+        this.addChild(sprite);
+
+        return sprite;
+    };
+
+    addBorder(object: Container) {
+        const border = new Graphics();
+        border.lineStyle(2, 0xFF0000);
+        border.drawRect(object.x - 1, object.y - 1, object.width + 2, object.height + 2);
+
+        this.addChild(border);
+    };
+
+    addInfoWindow() {
+        const infoWindow = new Container();
+
+        infoWindow.x = this.sprite.x + this.sprite.width;
+        infoWindow.y = this.sprite.y;
+
+        const infoText = new Text('Information goes here.');
+
+        infoWindow.addChild(infoText);
+        infoWindow.visible = false;
+
+        this.addChild(infoWindow);
+
+        return infoWindow;
+    };
+
+    onClickSprite = () => {
+        this.infoWindow.visible = true;
+    };
+
+    showinfo() {
+        console.log(`${this.title} coords: ${this.position.x}, ${this.position.y}`);
+    };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,50 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, FederatedPointerEvent, Sprite } from 'pixi.js';
+import { Room } from './Objects/Room';
+
+const defaultSize = {
+	width: 1080,
+	height: 720,
+};
+
+const canvasElement = document.getElementById("pixi-canvas") as HTMLCanvasElement;
+
+const rooms: Room[] = [];
+
+rooms.push(new Room('Room 1', 245, 225, 194, 76));
 
 const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+	view: canvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: defaultSize.width,
+	height: defaultSize.height,
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+const background: Sprite = Sprite.from("back.jpg");
+background.interactive = true;
+// background.width = defaultSize.width;
+// background.height = defaultSize.height;
 
-clampy.anchor.set(0.5);
+app.stage.addChild(background);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+for (const room of rooms) {
+	app.stage.addChild(room);
+}
 
-app.stage.addChild(clampy);
+app.stage.once('click', onClick);
+
+function onClick(event: FederatedPointerEvent) {
+	const target = event.target;
+
+	for (const room of rooms) {
+		const {infoWindow} = room;
+
+		if (infoWindow.visible) {
+			if (infoWindow !== target && room.sprite !== target) {
+				infoWindow.visible = false;
+				console.log(true);
+			}
+		}
+	}
+};
